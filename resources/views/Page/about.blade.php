960b3062728026f421ce8607e78ab9674d1f02c9
tuanpth1909 <60849085+tuanpth1909@users.noreply.github.com>
2020-07-30 01:42:58 +0700

    FIX : About | sua lai duong dan cho file

--- resources/views/Page/about.blade.php
+++ resources/views/Page/about.blade.php
@@ -2,16 +2,16 @@
 
 <!-- Style Main_style-->
 @section('style')
-    <link rel="stylesheet" type="text/css" href="source/styles/news.css">
-    <link rel="stylesheet" type="text/css" href="source/styles/news_responsive.css">
+    <link rel="stylesheet" type="text/css" href="source/styles/about.css">
+    <link rel="stylesheet" type="text/css" href="source/styles/about_responsive.css">
 @endsection
 
-{{--Content About--}}
-@section('About')
+<!-- Content Home -->
+@section('Content')
     <!-- Home -->
 
     <div class="home">
-        <div class="background_image" style="background-image:url(images/about.jpg)"></div>
+        <div class="background_image" style="background-image:url(source/images/about.jpg)"></div>
     </div>
 
     <!-- Search -->
@@ -60,7 +60,7 @@
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <div class="about_image"><img src="images/about_1.jpg" alt=""></div>
+                    <div class="about_image"><img src="source/images/about_1.jpg" alt=""></div>
                 </div>
             </div>
         </div>
@@ -75,7 +75,7 @@
                 <!-- Milestone -->
                 <div class="col-lg-3 milestone_col">
                     <div class="milestone text-center">
-                        <div class="milestone_icon"><img src="images/mountain.svg" alt=""></div>
+                        <div class="milestone_icon"><img src="source/images/mountain.svg" alt=""></div>
                         <div class="milestone_counter" data-end-value="17">0</div>
                         <div class="milestone_text">Online Courses</div>
                     </div>
@@ -84,7 +84,7 @@
                 <!-- Milestone -->
                 <div class="col-lg-3 milestone_col">
                     <div class="milestone text-center">
-                        <div class="milestone_icon"><img src="images/island.svg" alt=""></div>
+                        <div class="milestone_icon"><img src="source/images/island.svg" alt=""></div>
                         <div class="milestone_counter" data-end-value="213">0</div>
                         <div class="milestone_text">Students</div>
                     </div>
@@ -93,7 +93,7 @@
                 <!-- Milestone -->
                 <div class="col-lg-3 milestone_col">
                     <div class="milestone text-center">
-                        <div class="milestone_icon"><img src="images/camera.svg" alt=""></div>
+                        <div class="milestone_icon"><img src="source/images/camera.svg" alt=""></div>
                         <div class="milestone_counter" data-end-value="11923">0</div>
                         <div class="milestone_text">Teachers</div>
                     </div>
@@ -102,7 +102,7 @@
                 <!-- Milestone -->
                 <div class="col-lg-3 milestone_col">
                     <div class="milestone text-center">
-                        <div class="milestone_icon"><img src="images/boat.svg" alt=""></div>
+                        <div class="milestone_icon"><img src="source/images/boat.svg" alt=""></div>
                         <div class="milestone_counter" data-end-value="15">0</div>
                         <div class="milestone_text">Countries</div>
                     </div>
@@ -115,7 +115,7 @@
     <!-- Why Choose Us -->
 
     <div class="why">
-        <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/why.jpg" data-speed="0.8"></div>
+        <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="source/images/why.jpg" data-speed="0.8"></div>
         <div class="container">
             <div class="row">
                 <div class="col text-center">
@@ -129,9 +129,9 @@
                 <div class="col-lg-4 why_col">
                     <div class="why_item">
                         <div class="why_image">
-                            <img src="images/why_1.jpg" alt="">
+                            <img src="source/images/why_1.jpg" alt="">
                             <div class="why_icon d-flex flex-column align-items-center justify-content-center">
-                                <img src="images/why_1.svg" alt="">
+                                <img src="source/images/why_1.svg" alt="">
                             </div>
                         </div>
                         <div class="why_content text-center">
@@ -147,9 +147,9 @@
                 <div class="col-lg-4 why_col">
                     <div class="why_item">
                         <div class="why_image">
-                            <img src="images/why_2.jpg" alt="">
+                            <img src="source/images/why_2.jpg" alt="">
                             <div class="why_icon d-flex flex-column align-items-center justify-content-center">
-                                <img src="images/why_2.svg" alt="">
+                                <img src="source/images/why_2.svg" alt="">
                             </div>
                         </div>
                         <div class="why_content text-center">
@@ -165,9 +165,9 @@
                 <div class="col-lg-4 why_col">
                     <div class="why_item">
                         <div class="why_image">
-                            <img src="images/why_3.jpg" alt="">
+                            <img src="source/images/why_3.jpg" alt="">
                             <div class="why_icon d-flex flex-column align-items-center justify-content-center">
-                                <img src="images/why_3.svg" alt="">
+                                <img src="source/images/why_3.svg" alt="">
                             </div>
                         </div>
                         <div class="why_content text-center">
@@ -198,7 +198,7 @@
                 <!-- Team Item -->
                 <div class="col-xl-3 col-md-6 team_col">
                     <div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
-                        <div class="team_image"><img src="images/team_1.jpg" alt=""></div>
+                        <div class="team_image"><img src="source/images/team_1.jpg" alt=""></div>
                         <div class="team_content">
                             <div class="team_title"><a href="#">Margaret Smith</a></div>
                             <div class="team_text">
@@ -211,7 +211,7 @@
                 <!-- Team Item -->
                 <div class="col-xl-3 col-md-6 team_col">
                     <div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
-                        <div class="team_image"><img src="images/team_2.jpg" alt=""></div>
+                        <div class="team_image"><img src="source/images/team_2.jpg" alt=""></div>
                         <div class="team_content">
                             <div class="team_title"><a href="#">James Williams</a></div>
                             <div class="team_text">
@@ -224,7 +224,7 @@
                 <!-- Team Item -->
                 <div class="col-xl-3 col-md-6 team_col">
                     <div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
-                        <div class="team_image"><img src="images/team_3.jpg" alt=""></div>
+                        <div class="team_image"><img src="source/images/team_3.jpg" alt=""></div>
                         <div class="team_content">
                             <div class="team_title"><a href="#">Michael James</a></div>
                             <div class="team_text">
@@ -237,7 +237,7 @@
                 <!-- Team Item -->
                 <div class="col-xl-3 col-md-6 team_col">
                     <div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
-                        <div class="team_image"><img src="images/team_4.jpg" alt=""></div>
+                        <div class="team_image"><img src="source/images/team_4.jpg" alt=""></div>
                         <div class="team_content">
                             <div class="team_title"><a href="#">Noah Smith</a></div>
                             <div class="team_text">
@@ -253,6 +253,5 @@
 @endsection
 
 @section('script')
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCIwF204lFZg1y4kPSIhKaHEXMLYxxuMhA"></script>
-    <script src="source/js/contact.js"></script>
+    <script src="source/js/about.js"></script>
 @endsection
