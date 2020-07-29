5edaee5fe40d0430ab5fbc053161a3a962717bf9
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-07-29 22:21:13 +0700

    FIX: master-page, Nav bar

--- resources/views/Page/Contact.blade.php
+++ resources/views/Page/Contact.blade.php
@@ -7,25 +7,53 @@
 @endsection
 
 <!-- Menu Nav Header-->
-@section('Nav')
+@section('nav')
     <nav class="main_nav">
         <ul class="d-flex flex-row align-items-start justify-content-start">
-            <li ><a href="index.html">Home</a></li>
-            <li><a href="about.html">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="news.html">News</a></li>
-            <li class="active"><a href="contact.html">Contact</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="about">About us</a></li>
+            <li><a href="services">Services</a></li>
+            <li><a href="news">News</a></li>
+            <li class="active"><a href="contact">Contact</a></li>
         </ul>
     </nav>
 @endsection
 
-@section('Background_header')
+<!-- Content Contact -->
+@section('Content')
     <div class="home">
         <div class="background_image" style="background-image:url(source/images/contact.jpg)"></div>
     </div>
-@endsection
 
-@section('Content')
+    <!-- Search -->
+
+    <div class="home_search">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="home_search_container">
+                        <div class="home_search_title">Search for your trip</div>
+                        <div class="home_search_content">
+                            <form action="#" class="home_search_form" id="home_search_form">
+                                <div
+                                    class="d-flex flex-lg-row flex-column align-items-start justify-content-lg-between justify-content-start">
+                                    <input type="text" class="search_input search_input_1" placeholder="City"
+                                           required="required">
+                                    <input type="text" class="search_input search_input_2" placeholder="Departure"
+                                           required="required">
+                                    <input type="text" class="search_input search_input_3" placeholder="Arrival"
+                                           required="required">
+                                    <input type="text" class="search_input search_input_4" placeholder="Budget"
+                                           required="required">
+                                    <button class="home_search_button">search</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Contact -->
 
@@ -38,7 +66,9 @@
                     <div class="contact_content">
                         <div class="contact_title">Get in touch with us</div>
                         <div class="contact_text">
-                            <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo. Vivamus massa.Tempor massa et laoreet. Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo. Vivamus massa.</p>
+                            <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo. Vivamus
+                                massa.Tempor massa et laoreet. Pellentesque sit amet elementum ccumsan sit amet mattis
+                                eget, tristique at leo. Vivamus massa.</p>
                         </div>
                         <div class="contact_list">
                             <ul>
@@ -63,16 +93,29 @@
                 <div class="col-lg-6">
                     <div class="contact_form_container">
                         <form action="#" id="contact_form" class="contact_form">
-                            <div style="margin-bottom: 18px"><input type="text" class="contact_input contact_input_name inpt" placeholder="Your Name" required="required"><div class="input_border"></div></div>
+                            <div style="margin-bottom: 18px"><input type="text"
+                                                                    class="contact_input contact_input_name inpt"
+                                                                    placeholder="Your Name" required="required">
+                                <div class="input_border"></div>
+                            </div>
                             <div class="row">
                                 <div class="col-lg-6" style="margin-bottom: 18px">
-                                    <div><input type="text" class="contact_input contact_input_email inpt" placeholder="Your E-mail" required="required"><div class="input_border"></div></div>
+                                    <div><input type="text" class="contact_input contact_input_email inpt"
+                                                placeholder="Your E-mail" required="required">
+                                        <div class="input_border"></div>
+                                    </div>
                                 </div>
                                 <div class="col-lg-6" style="margin-bottom: 18px">
-                                    <div><input type="text" class="contact_input contact_input_subject inpt" placeholder="Subject" required="required"><div class="input_border"></div></div>
+                                    <div><input type="text" class="contact_input contact_input_subject inpt"
+                                                placeholder="Subject" required="required">
+                                        <div class="input_border"></div>
+                                    </div>
                                 </div>
                             </div>
-                            <div><textarea class="contact_textarea contact_input inpt" placeholder="Message" required="required"></textarea><div class="input_border" style="bottom:3px"></div></div>
+                            <div><textarea class="contact_textarea contact_input inpt" placeholder="Message"
+                                           required="required"></textarea>
+                                <div class="input_border" style="bottom:3px"></div>
+                            </div>
                             <button class="contact_button">send</button>
                         </form>
                     </div>
@@ -93,9 +136,9 @@
             </div>
         </div>
     </div>
+@endsection
 
-    @section('script')
+@section('script')
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCIwF204lFZg1y4kPSIhKaHEXMLYxxuMhA"></script>
     <script src="source/js/contact.js"></script>
-    @endsection
 @endsection
