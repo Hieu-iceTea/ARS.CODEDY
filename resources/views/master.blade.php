a7fe246eb55d69084339a6d97c19c38049b80487
= <60811767+truong-thanh-tu@users.noreply.github.com>
2020-07-31 12:19:48 +0700

    Fix: Sua lai duong dan

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -201,15 +201,15 @@
     </footer>
 
 </div>
-
-<script src="source/js/jquery-3.2.1.min.js"></script>
-<script src="source/styles/bootstrap4/popper.js"></script>
-<script src="source/styles/bootstrap4/bootstrap.min.js"></script>
-<script src="source/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-<script src="source/plugins/Isotope/isotope.pkgd.min.js"></script>
-<script src="source/plugins/scrollTo/jquery.scrollTo.min.js"></script>
-<script src="source/plugins/easing/easing.js"></script>
-<script src="source/plugins/parallax-js-master/parallax.min.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+<script src="{{ asset('styles/bootstrap4/popper.js') }}"></script>
+<script src="{{ asset('styles/bootstrap4/bootstrap.min.js') }}"></script>
+<script src="{{ asset('plugins/OwlCarousel2-2.2.1/owl.carousel.js') }}"></script>
+<script src="{{ asset('plugins/Isotope/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('plugins/scrollTo/jquery.scrollTo.min.js') }}"></script>
+<script src="{{ asset('plugins/easing/easing.js') }}"></script>
+<script src="{{ asset('plugins/parallax-js-master/parallax.min.js') }}"></script>
 @yield('script')
 
 </body>
