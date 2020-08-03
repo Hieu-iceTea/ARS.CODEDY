f8a8f58569aa1abb2fdb11b1e14b4bd5b71b2d21
Practice-KhongTen <59854810+Practice-KhongTen@users.noreply.github.com>
2020-08-03 15:59:37 +0700

    Update: VIet content cho trang news

--- resources/views/Page/News.blade.php
+++ resources/views/Page/News.blade.php
@@ -54,20 +54,20 @@
 
                         <!-- News Post -->
                         <div class="news_post">
-                            <div class="news_post_image"><img src="source/images/news_4.jpg" alt=""></div>
+                            <div class="news_post_image"><img src="{{ asset('source/images/news1.jpg') }}" alt=""></div>
                             <div class="news_post_content">
                                 <div class="news_post_date d-flex flex-row align-items-end justify-content-start">
-                                    <div>02</div>
-                                    <div>june</div>
+                                    <div>1</div>
+                                    <div>August</div>
                                 </div>
-                                <div class="news_post_title"><a href="#">Best tips to travel light</a></div>
+                                <div class="news_post_title"><a href="#">SIBENIK AND THE KORNATI ISLANDS, CROATIA</a></div>
                                 <div class="news_post_category">
                                     <ul>
                                         <li><a href="#">lifestyle & travel</a></li>
                                     </ul>
                                 </div>
                                 <div class="news_post_text">
-                                    <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo. Vivamus massa.Tempor massa et laoreet. Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo. Vivamus massa.Tempor massa et laoreet.</p>
+                                    <p>Croatia, believe it or not, is still actually an option, even though its picture-perfect medieval cities and extraordinary coastline welcomed 20 million visitors last year. And one sometimes suspects that all 20 million turn up in August, when those straight-out-of-a-fantasy-novel fortified towns are bursting at the seams. Thank heavens, then, for Obonjan. This wellness retreat from the team behind the Hideout festival, sprawling across a private island of the same name, got off to a shaky start when it launched in 2016. But the kinks have been completely ironed out, and guests are guaranteed a tempting mix of yoga classes, chill-out music and glamping. Spare some time as well for Sibenik, a short speedboat ride away: a major historic city and a lovely labyrinth of chalk-white stone. A day’s sailing around the Kornati Islands – unpopulated idylls given over to vineyards, orchards, and little else – also supplies some much-needed mellow.</p>
                                 </div>
                                 <div class="news_post_link"><a href="#">Read More</a></div>
                             </div>
@@ -75,20 +75,20 @@
 
                         <!-- News Post -->
                         <div class="news_post">
-                            <div class="news_post_image"><img src="source/images/news_5.jpg" alt=""></div>
+                            <div class="news_post_image"><img src="{{ asset('source/images/news2.jpg') }}" alt=""></div>
                             <div class="news_post_content">
                                 <div class="news_post_date d-flex flex-row align-items-end justify-content-start">
-                                    <div>02</div>
-                                    <div>june</div>
+                                    <div>2</div>
+                                    <div>October</div>
                                 </div>
-                                <div class="news_post_title"><a href="#">10 Amazing Destination for you this summer</a></div>
+                                <div class="news_post_title"><a href="#"> Changdeokgung Palace In Seoul </a></div>
                                 <div class="news_post_category">
                                     <ul>
                                         <li><a href="#">lifestyle & travel</a></li>
                                     </ul>
                                 </div>
                                 <div class="news_post_text">
-                                    <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo. Vivamus massa.Tempor massa et laoreet. Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo. Vivamus massa.Tempor massa et laoreet.</p>
+                                    <p> A must see spot for autumn leaves in Seoul. Not only does this historic attraction offer a glimpse of traditional Korea, it’s also packed full of leafy trees that shower the palace grounds with golden leaves. See the fall foliage atop the curved palace rooftops, or scattered along the pathway, offering up plenty of places to take memorable pictures wearing traditional Korean hanbok..</p>
                                 </div>
                                 <div class="news_post_link"><a href="#">Read More</a></div>
                             </div>
@@ -99,17 +99,17 @@
                             <div class="news_post_image"><img src="source/images/news_6.jpg" alt=""></div>
                             <div class="news_post_content">
                                 <div class="news_post_date d-flex flex-row align-items-end justify-content-start">
-                                    <div>02</div>
-                                    <div>june</div>
+                                    <div>3</div>
+                                    <div>weekend</div>
                                 </div>
-                                <div class="news_post_title"><a href="#">How to organize your perfect vacation</a></div>
+                                <div class="news_post_title"><a href="#">How to Have a Good Weekend</a></div>
                                 <div class="news_post_category">
                                     <ul>
                                         <li><a href="#">lifestyle & travel</a></li>
                                     </ul>
                                 </div>
                                 <div class="news_post_text">
-                                    <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo. Vivamus massa.Tempor massa et laoreet. Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo. Vivamus massa.Tempor massa et laoreet.</p>
+                                    <p>Weekends are an important break from the regular work week, giving you time to relax and do things that you're interested in. Try to take part in a stress-relieving activity — such as exercising, spending time outside, or disconnecting from social media — while also dedicating some time to your passions or hobbies. If you need to get some work done over the weekend, set a time limit and make sure your to-do list is reasonable.</p>
                                 </div>
                                 <div class="news_post_link"><a href="#">Read More</a></div>
                             </div>
@@ -153,27 +153,27 @@
 
                                 <!-- Latest Post -->
                                 <div class="latest_post d-flex flex-row align-items-start justify-content-start">
-                                    <div class="latest_post_image"><img src="source/images/latest_1.jpg" alt=""></div>
+                                    <div class="latest_post_image"><img src="{{ asset('source/images/news1.jpg') }}" alt=""></div>
                                     <div class="latest_post_content">
                                         <div class="latest_post_date d-flex flex-row align-items-end justify-content-start">
-                                            <div class="latest_post_day">02</div>
-                                            <div class="latest_post_month">june</div>
+                                            <div class="latest_post_day">1</div>
+                                            <div class="latest_post_month">August</div>
                                         </div>
-                                        <div class="latest_post_title"><a href="#">Best tips to travel light</a></div>
-                                        <div class="latest_post_text"><p>Pellentesque sit amet..</p></div>
+                                        <div class="latest_post_title"><a href="#">SIBENIK AND THE KORNATI ISLANDS, CROATIA</a></div>
+                                        <div class="latest_post_text"><p>Croatia, believe it or not..</p></div>
                                     </div>
                                 </div>
 
                                 <!-- Latest Post -->
                                 <div class="latest_post d-flex flex-row align-items-start justify-content-start">
-                                    <div class="latest_post_image"><img src="source/images/latest_2.jpg" alt=""></div>
+                                    <div class="latest_post_image"><img src="{{ asset('source/images/news2.jpg') }}" alt=""></div>
                                     <div class="latest_post_content">
                                         <div class="latest_post_date d-flex flex-row align-items-end justify-content-start">
-                                            <div class="latest_post_day">02</div>
-                                            <div class="latest_post_month">june</div>
+                                            <div class="latest_post_day">2</div>
+                                            <div class="latest_post_month">October</div>
                                         </div>
-                                        <div class="latest_post_title"><a href="#">Best tips to travel light</a></div>
-                                        <div class="latest_post_text"><p>Pellentesque sit amet..</p></div>
+                                        <div class="latest_post_title"><a href="#">Changdeokgung Palace In Seoul</a></div>
+                                        <div class="latest_post_text"><p>A must see spot for autumn leaves..</p></div>
                                     </div>
                                 </div>
 
@@ -182,11 +182,11 @@
                                     <div class="latest_post_image"><img src="source/images/latest_3.jpg" alt=""></div>
                                     <div class="latest_post_content">
                                         <div class="latest_post_date d-flex flex-row align-items-end justify-content-start">
-                                            <div class="latest_post_day">02</div>
-                                            <div class="latest_post_month">june</div>
+                                            <div class="latest_post_day">3</div>
+                                            <div class="latest_post_month">Weekends</div>
                                         </div>
-                                        <div class="latest_post_title"><a href="#">Best tips to travel light</a></div>
-                                        <div class="latest_post_text"><p>Pellentesque sit amet..</p></div>
+                                        <div class="latest_post_title"><a href="#">How to Have a Good Weekend</a></div>
+                                        <div class="latest_post_text"><p>Weekends are an important break..</p></div>
                                     </div>
                                 </div>
 
