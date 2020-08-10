f2525b0da11e6cfebc17a6eea4beb556071d8113
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-08-10 15:57:34 +0700

    Update ARS_ScriptDatabe_Create_Insert.sql

--- _Docs/DataBase_Script/ARS_ScriptDatabe_Create_Insert.sql
+++ _Docs/DataBase_Script/ARS_ScriptDatabe_Create_Insert.sql
@@ -1,34 +1,54 @@
-# - - - - - - - - - - - - - - - - - - - - Create DataBase - - - - - - - - - - - - - - - - - - - -  #
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+#                                           Create DataBase                                           #
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
 
-# Create DataBase -> ARS
-DROP DATABASE IF EXISTS ARS;
-CREATE DATABASE IF NOT EXISTS ARS;
+# Create DataBase
+DROP DATABASE IF EXISTS `ARS_CODEDY`;
+CREATE DATABASE IF NOT EXISTS `ARS_CODEDY` CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-USE ARS;
+USE `ARS_CODEDY`;
+SET time_zone = '+7:00';
 
-# - - - - - - - - - - - - - - - - - - - - Create Tables - - - - - - - - - - - - - - - - - - - -  #
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+#                                            Create Tables                                            #
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
 
-# Create Table -> CreditCard
-DROP TABLE IF EXISTS CreditCards;
-CREATE TABLE IF NOT EXISTS CreditCards (
-    ID INT AUTO_INCREMENT,
+# Create Table
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+    `user_id` INT AUTO_INCREMENT,
 
-    Name VARCHAR(64),
-    Number  INT(64),
-    CVV  INT(16),
-    ExpirationMonth  INT(2),
-    ExpirationYear  INT(2),
+    `user_name` VARCHAR(64) NOT NULL,
+    `email` VARCHAR(64) NOT NULL,
+    `password` VARCHAR(128) NOT NULL,
+    `level`  TINYINT UNSIGNED NOT NULL,
+    `email_verified_at` DATETIME,
+    `verification_code` VARCHAR(8),
+    `remember_token` VARCHAR(128),
 
-    Created_By NVARCHAR(32) DEFAULT 'Hieu-iceTea',
-    Created_At DATETIME DEFAULT CURRENT_TIME,
-    Updated_By NVARCHAR(32) DEFAULT NULL,
-    Updated_At DATETIME DEFAULT NULL,
-    Version INT DEFAULT 1,
-    Enabled BOOLEAN DEFAULT TRUE,
+    `gender` BOOLEAN NOT NULL,
+    `first_name` VARCHAR(64),
+    `last_name` VARCHAR(64),
+    `phone` VARCHAR(16),
+    `address` VARCHAR(128),
 
-    PRIMARY KEY (ID)
+    `created_by` NVARCHAR(32) DEFAULT 'Hieu-iceTea',
+    `created_at` DATETIME DEFAULT CURRENT_TIME,
+    `updated_by` NVARCHAR(32) DEFAULT NULL,
+    `updated_at` DATETIME DEFAULT NULL,
+    `version` INT DEFAULT 1,
+    `deleted` BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY (`user_id`)
 ) ENGINE InnoDB;
 
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+#                                             Insert Data                                             #
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
 
-# - - - - - - - - - - - - - - - - - - - - Insert Data - - - - - - - - - - - - - - - - - - - -  #
-# Phần này Hiếu sẽ viết Script Insert sau, hoặc hướng dẫn AE tự viết! [Dùng excel đổ data cho nhanh]
+#Password: 123456
+INSERT INTO user (user_name, email, password, level, email_verified_at, verification_code, remember_token, gender, first_name, last_name, phone, address)
+VALUES ('user_name', 'email', '$2y$10$YKY51A9REcXLZVRAC87AcuXnC.Nb8WK8rD/WgfAVxPSAelLZHQf06', 0, '2020-08-08', NULL, NULL, 0, 'first_name', 'last_name', 'phone', 'address');
+
+
+
