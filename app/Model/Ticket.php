ae38f0f93e679f6948abe2ab60d2e3a2efaeeaad
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-08-11 17:24:13 +0700

    Update Ticket.php

--- app/Model/Ticket.php
+++ app/Model/Ticket.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $table = 'ticket';
+    protected $primaryKey = 'ticket_id';
+    protected $guarded = [];
+    protected $perPage = 5;
+
+    public static function all($columns = ['*'])
+    {
+        return parent::all($columns)->where('deleted', false);
+    }
 }
