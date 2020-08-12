3b1ff2146e62efaac27e97644a10e664f1a15d13
= <60811767+truong-thanh-tu@users.noreply.github.com>
2020-08-13 05:02:50 +0700

    Update TicketController.php

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 use App\Model\Ticket;
 use App\Model\Airport;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
 
 class TicketController extends Controller
 {
