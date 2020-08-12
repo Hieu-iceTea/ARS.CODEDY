0f181d9626f3df9a527b4b1778d3528a2998cd40
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-08-12 16:26:06 +0700

    Create Utility.php

---
+++ app/Utilities/Utility.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Utilities;
+
+
+class Utility
+{
+    public $status_ticket = [
+        1 => 'Unverified',
+        2 => 'Reservations',
+        3 => 'Paid',
+        4 => 'Finish',
+    ];
+}
