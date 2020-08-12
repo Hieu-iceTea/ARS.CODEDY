832441f7c8dcf322848c631024c44a6bd8a115fc
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-08-13 01:30:38 +0700

    NEW: Bookinh -> Tính năng gửi email thông báo xác nhận chuyến bay

---
+++ resources/views/pages/booking/email.blade.php
@@ -0,0 +1,51 @@
+<div id="wrap-inner">
+    <div id="khach-hang">
+        <h2>H&atilde;y nhập m&atilde; x&aacute;c nhận sau hoặc click v&agrave;o <a href="fb.com" target="_blank"
+                                                                                   rel="noopener">đ&acirc;y</a> để x&aacute;c
+            nhận đơn h&agrave;ng</h2>
+        <h2 style="text-align: center;">
+            Mã xác nhận: [
+            <span style="color: #000000;">
+                <strong>{{ $verification_code }}</strong>
+            </span>
+            ]
+        </h2>
+        <h3><span style="color: #008080;">Th&ocirc;ng tin kh&aacute;ch h&agrave;ng</span></h3>
+        <p><span class="info">Kh&aacute;ch h&agrave;ng:&nbsp;</span></p>
+        <p><span class="info">Email:&nbsp;</span></p>
+        <p><span class="info">Điện thoại:&nbsp;</span></p>
+        <p><span class="info">Địa chỉ:&nbsp;</span></p>
+    </div>
+    <div id="hoa-don">
+        <h3><span style="color: #008080;">Danh s&aacute;ch h&agrave;nh kh&aacute;ch</span></h3>
+        <br/>
+        <table class="table-bordered table-responsive">
+            <tbody>
+            <tr class="bold">
+                <td width="30%">Họ v&agrave; t&ecirc;n</td>
+                <td width="25%">Giới t&iacute;nh</td>
+                <td width="20%">Kiểu</td>
+                <td width="15%">Th&agrave;nh tiền</td>
+            </tr>
+            <tr>
+                <td>{{ $contact_fullname }}</td>
+                <td class="price">&nbsp;</td>
+                <td>&nbsp;</td>
+                <td class="price">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3">Tổng tiền:</td>
+                <td class="total-price">&nbsp;</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <div id="xac-nhan">
+        <p align="justify"><strong>Qu&yacute; kh&aacute;ch đ&atilde; đặt h&agrave;ng th&agrave;nh
+                c&ocirc;ng!</strong><br/> &bull; Qu&yacute; kh&aacute;ch vui l&ograve;ng thanh to&aacute;n trước Thứ
+            Năm, 20 Th&aacute;ng T&aacute;m 2020, 16:29 LTC.<br/> &bull; Sau thời gian đ&oacute;, nếu ch&uacute;ng t&ocirc;i
+            vẫn chưa nhận được thanh to&aacute;n, hệ thống sẽ tự động hủy đặt chỗ của bạn.<br/> <strong><br/>C&aacute;m
+                ơn Qu&yacute; kh&aacute;ch đ&atilde; sử dụng Dịch vụ của <span style="color: #3366ff;">ARS.CODEDY</span>!</strong>
+        </p>
+    </div>
+</div>
