31894fe02f29514e65ffa8f372f222f8d5e44291
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-08-12 16:29:14 +0700

    Update Utility.php

--- app/Utilities/Utility.php
+++ app/Utilities/Utility.php
@@ -6,7 +6,7 @@ namespace App\Utilities;
 
 class Utility
 {
-    public $status_ticket = [
+    public static $ticket_status = [
         1 => 'Unverified',
         2 => 'Reservations',
         3 => 'Paid',
