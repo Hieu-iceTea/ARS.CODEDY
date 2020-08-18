eee32cd2bb8c7cac1c0ac6a40469aa67e8275f81
Nguyễn Đình Hiếu <51254371+Hieu-iceTea@users.noreply.github.com>
2020-08-18 10:56:17 +0700

    Update my_script.js

--- public/js/my_script.js
+++ public/js/my_script.js
@@ -1,6 +1,6 @@
 /* Preloader */
 $(window).on('load', function () {
-    $('#preloader-active').delay(4500).fadeOut('slow');
+    $('#preloader-active').delay(450).fadeOut('slow');
     $('body').delay(450).css({
         'overflow': 'visible'
     });
@@ -73,36 +73,7 @@ function change($value) {
     }
 }
 
-function preloaderActive() {
+function preloaderActive(time = 250) {
     $('#preloader-active').delay(0).fadeIn(10);
-    $('#preloader-active').delay(300).fadeOut('slow');
-}
-
-function setValue(flight_schedule_id, seat_type, seat_price, adults, children, infant) {
-    preloaderActive();
-
-    //set value to hidden-field input
-    document.getElementById('flight_schedule_id').value = flight_schedule_id;
-    document.getElementById('seat_type').value = seat_type;
-    document.getElementById('seat_price').value = seat_price;
-    document.getElementById('adults').value = adults;
-    document.getElementById('children').value = children;
-    document.getElementById('infant').value = infant;
-    document.getElementById('total_passenger').value = adults + children + infant;
-
-    //set value to label info
-    // document.getElementById('sub_total').innerText = seat_price.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-    const sub_totals = document.getElementsByClassName('sub_total');
-    for (const sub_total of sub_totals) {
-        sub_total.innerText = seat_price.toLocaleString("vi-vn");
-    }
-
-    const total_price_adults = adults * seat_price;
-    const total_price_children = children * seat_price;
-    const total_price_infant = infant * seat_price;
-    const total_price = total_price_adults + total_price_children + total_price_infant;
-    document.getElementById('total_price_adults').innerText = total_price_adults.toLocaleString("vi-vn");
-    document.getElementById('total_price_children').innerText = total_price_children.toLocaleString("vi-vn");
-    document.getElementById('total_price_infant').innerText = total_price_infant.toLocaleString("vi-vn");
-    document.getElementById('total_price').innerText = total_price.toLocaleString("vi-vn");
+    $('#preloader-active').delay(time).fadeOut('slow');
 }
