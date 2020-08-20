66af8d5e69c51891d12f9e950ca49e0e3d56bb41
vuquanghuy2001 <59812614+vuquanghuy2001@users.noreply.github.com>
2020-08-20 10:54:58 +0700

    UPDATE: sửa nội dung trang about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -40,24 +40,25 @@
             <div class="row about_row">
                 <div class="col-lg-6">
                     <div class="about_content">
-                        <div class="text_highlight">Pellentesque sit amet elementum ccumsan sit amet mattis eget,
-                            tristique at leo. Vivamus massa.Tempor massa et laoreet .Lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit.
+                        <div class="text_highlight">ARS.CODEDY, a four-star airline, is one of the most prestigious and
+                            quality airlines in Vietnam, always chosen by passengers as a companion.
                         </div>
                         <div class="about_text">
-                            <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo. Vivamus
-                                massa.Tempor massa et laoreet .Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Sed eu laoreet ante, sollicitudin volutpat quam. Vestibulum posuere malesuada ultrices.
-                                In pulvinar rhoncus lacus at aliquet. Nunc vitae lacus varius, auctor nisi sit amet,
-                                consectetur mauris. Curabitur sodales semper est, vel faucibus urna laoreet vel. Ut
-                                justo diam, sodales non pulvinar at, vulputate quis neque. Etiam aliquam purus vel
-                                ultricies consequat.</p>
+                            <p>From 1976 to 1980, ARS.CODEDY expanded and effectively exploited many international
+                                routes to Asian countries. In April 1993, ARS.CODEDY Airlines was officially formed as
+                                a large-scale air transportation business unit of the State. In October 2002, Vietnam
+                                Airlines introduced a new logo, demonstrating the development of ARS.CODEDY to become
+                                an airline with a caliber and identity in the region and in the world. In October 2003,
+                                ARS.CODEDY began to receive and put into operation a modern aircraft with many
+                                outstanding features of Boeing 777 first among 6 Boeing 777 ordered by Boeing.
+                                This event marked the start of the airline's modernization program. Today, ARS.CODEDY
+                                has become one of the youngest and most modern airlines in the region with an average fleet age of 5.4 years.</p>
                         </div>
                         <div class="button about_button"><a href="#">read more</a></div>
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <div class="about_image"><img src="img/about_1.jpg" alt=""></div>
+                    <div class="about_image"><img src="img/maybay.jpg" alt=""></div>
                 </div>
             </div>
         </div>
@@ -117,7 +118,7 @@
         <div class="container">
             <div class="row">
                 <div class="col text-center">
-                    <div class="section_subtitle">simply amazing places</div>
+                    <div class="section_subtitle">best service</div>
                     <div class="section_title"><h2>Why choose us?</h2></div>
                 </div>
             </div>
@@ -135,7 +136,7 @@
                         <div class="why_content text-center">
                             <div class="why_title">Fast Services</div>
                             <div class="why_text">
-                                <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo.</p>
+                                <p>With 4-star standard we always bring satisfaction to customers about flight experience.</p>
                             </div>
                         </div>
                     </div>
@@ -153,7 +154,7 @@
                         <div class="why_content text-center">
                             <div class="why_title">Great Team</div>
                             <div class="why_text">
-                                <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo.</p>
+                                <p>Our team of pilots and flight attendants with top experience will give customers the safest flight.</p>
                             </div>
                         </div>
                     </div>
@@ -169,9 +170,9 @@
                             </div>
                         </div>
                         <div class="why_content text-center">
-                            <div class="why_title">Best Deals</div>
+                            <div class="why_title">Promotion</div>
                             <div class="why_text">
-                                <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo.</p>
+                                <p>We always offer attractive promotions for customers, with cheap flights and diverse flight schedules.</p>
                             </div>
                         </div>
                     </div>
@@ -196,12 +197,11 @@
                 <!-- Team Item -->
                 <div class="col-xl-3 col-md-6 team_col">
                     <div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
-                        <div class="team_image"><img src="img/team_1.jpg" alt=""></div>
+                        <div class="team_image"><img src="img/hieu.jpg" alt=""></div>
                         <div class="team_content">
-                            <div class="team_title"><a href="#">Margaret Smith</a></div>
+                            <div class="team_title"><a href="#">Dinh Hieu</a></div>
                             <div class="team_text">
-                                <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo.
-                                    Vivamus massa.</p>
+                                <h5>Business Head</h5>
                             </div>
                         </div>
                     </div>
@@ -210,12 +210,11 @@
                 <!-- Team Item -->
                 <div class="col-xl-3 col-md-6 team_col">
                     <div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
-                        <div class="team_image"><img src="img/team_2.jpg" alt=""></div>
+                        <div class="team_image"><img src="img/hoa.jpg" alt=""></div>
                         <div class="team_content">
-                            <div class="team_title"><a href="#">James Williams</a></div>
+                            <div class="team_title"><a href="#">Chan Hoa</a></div>
                             <div class="team_text">
-                                <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo.
-                                    Vivamus massa.</p>
+                                <h5>CEO & Founder</h5>
                             </div>
                         </div>
                     </div>
@@ -224,12 +223,11 @@
                 <!-- Team Item -->
                 <div class="col-xl-3 col-md-6 team_col">
                     <div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
-                        <div class="team_image"><img src="img/team_3.jpg" alt=""></div>
+                        <div class="team_image"><img src="img/huy.jpg" alt=""></div>
                         <div class="team_content">
-                            <div class="team_title"><a href="#">Michael James</a></div>
+                            <div class="team_title"><a href="#">Quang Huy</a></div>
                             <div class="team_text">
-                                <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo.
-                                    Vivamus massa.</p>
+                                <h5>Web Deverloper</h5>
                             </div>
                         </div>
                     </div>
@@ -238,12 +236,11 @@
                 <!-- Team Item -->
                 <div class="col-xl-3 col-md-6 team_col">
                     <div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
-                        <div class="team_image"><img src="img/team_4.jpg" alt=""></div>
+                        <div class="team_image"><img src="img/tuan.jpg" alt=""></div>
                         <div class="team_content">
-                            <div class="team_title"><a href="#">Noah Smith</a></div>
+                            <div class="team_title"><a href="#">Pham Tuan</a></div>
                             <div class="team_text">
-                                <p>Pellentesque sit amet elementum ccumsan sit amet mattis eget, tristique at leo.
-                                    Vivamus massa.</p>
+                                <h5>UI/UX Designer</h5>
                             </div>
                         </div>
                     </div>
