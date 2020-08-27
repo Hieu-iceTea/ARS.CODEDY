a787f91eab6ebcce969aba2d3f5318c52e788b64
tuanpth1909 <60849085+tuanpth1909@users.noreply.github.com>
2020-08-27 14:30:44 +0700

    NEW : Index cuâ trang Promotion
    Đã bao gồm cả chức năng xóa

--- app/Http/Controllers/Admin/PromotionController.php
+++ app/Http/Controllers/Admin/PromotionController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Model\Promotion;
 use Illuminate\Http\Request;
+
 
 class PromotionController extends Controller
 {
@@ -11,9 +13,36 @@ class PromotionController extends Controller
      * Display a listing of the resource.
      *
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.promotion.index');
+        $keyword = $request->get('search');
+
+        $promotions = Promotion::where('code', '=', $keyword)
+            // Tìm kiếm theo tên được search
+            ->orWhere('title', 'like', '%' . $keyword . '%')
+            ->orWhere('expiration_date', 'like', '%' . $keyword . '%')
+            //Sắp xép theo thứ tự id
+            ->orderBy('promotion_id', 'desc')
+            ->paginate();
+
+        //giúp chuyển trang page sẽ đính kèm theo từ khóa search của người dùng:
+        $promotions->appends(['search' => $keyword]);
+
+        return view('admin.promotion.index',compact('promotions'));
+//------------------------------
+//        $keyword = $request->get('search');
+//
+//        if ($keyword != '') {
+//            $promotions = Promotion::search($keyword);
+//
+//            if (count($promotions) > 0) {
+//                return view('admin.promotion.index', compact('promotions'));
+//            }
+//            return view('admin.promotion.index', compact('promotions'))->withErrors('No search results. Try to search again!');
+//        }
+//
+//        $promotions = Promotion::all();
+//        return view('admin.promotion.index', compact('promotions'));
     }
 
     /**
@@ -79,6 +108,19 @@ class PromotionController extends Controller
      */
     public function destroy($id)
     {
-        //
+//        Promotion::where('promotion_id', $id)->update([
+//            'deleted' => true,
+//        ]);
+//
+//        if (url()->previous() != url()->current()) {
+//            return redirect()->back()->with('notification', 'Deleted successfully!');
+//        } else {
+//            //Nếu trang trước giống trang hiện tại thì chuyển hướng về trang chủ
+//            //(tức là ở trang detail, ấn nút xóa. xóa xong redirect()->back() sẽ quay lại trang detail để hiện thị item đã xóa gây ra lỗi)
+//            return redirect('admin/promotion')->with('notification', 'Deleted successfully!');
+//        }
+        Promotion::find($id)->delete();
+        return redirect('admin/promotion')->with('notification', 'Deleted successfully!');
+
     }
 }
--- resources/views/admin/promotion/index.blade.php
+++ resources/views/admin/promotion/index.blade.php
@@ -3,5 +3,167 @@
 @section('title', 'Promotion')
 
 @section('main')
-    Promotion
+    <div class="app-main__inner">
+        <div class="app-page-title">
+            <div class="page-title-wrapper">
+
+                <div class="page-title-heading">
+                    <div class="page-title-icon">
+                        <i class="pe-7s-ticket icon-gradient bg-mean-fruit">
+                        </i>
+                    </div>
+                    <div>Promotion Management
+                        <div class="page-title-subheading">
+                            View, create, update, delete and manage promotion.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="page-title-actions">
+                    <a href="{{ url()->current() . '/create' }}" data-toggle="tooltip"
+                       data-placement="bottom"
+                       class="btn-shadow btn-hover-shine mr-3 btn btn-primary">
+                        <span class="btn-icon-wrapper pr-2 opacity-7">
+                            <i class="fa fa-plus fa-w-20"></i>
+                        </span>
+                        New Promotion
+                    </a>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="main-card mb-3 card">
+
+                    <div class="card-header">
+
+                        <form>
+                            <div class="input-group">
+                                <input name="search" type="search" value="{{ request('search') }}" placeholder="Search everything"
+                                       class="form-control">
+                                <span class="input-group-append">
+                                        <button type="submit" class="btn btn-primary">
+                                            <i class="fa fa-search"></i>&nbsp;
+                                        </button>
+                                    </span>
+                            </div>
+                        </form>
+
+                        <div class="btn-actions-pane-right">
+                            <div role="group" class="btn-group-sm btn-group">
+                                <button class="active btn btn-focus">This week</button>
+                                <button class="btn btn-focus">Anytime</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="table-responsive">
+                        <table class="align-middle mb-0 table table-borderless table-striped table-hover" style="overflow: hidden;">
+                            <thead>
+                            <tr>
+                                <th class="text-center">#CODE</th>
+                                <th>Title</th>
+                                <th class="text-center">Discount</th>
+                                <th class="text-center">Qty</th>
+                                <th class="text-center">Expiration Date</th>
+                                <th class="text-center">Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            @foreach($promotions as $promotion)
+                                <tr>
+                                    {{--#code--}}
+                                    <td class="text-center text-muted">#{{$promotion->code}}</td>
+                                    {{--Title--}}
+                                    <td>
+                                        <div class="widget-content p-0">
+                                            <div class="widget-content-wrapper">
+                                                <div class="widget-content-left flex2">
+                                                    <div class="widget-subheading opacity-10 widget-heading">
+                                                        {{$promotion->title}}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    {{--Discount--}}
+                                    <td class="text-center">
+                                        <div class="widget-content p-0">
+                                            <div class="widget-content-wrapper">
+                                                <div class="widget-content-left flex2">
+                                                    {{ number_format($promotion->discount, 0, ',', '.') }} VND
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    {{--Qty--}}
+                                    <td class="text-center">
+                                        <div class="widget-content p-0">
+                                            <div class="widget-content-wrapper">
+                                                <div class="widget-content-left flex2">
+                                                    {{$promotion->qty_remain}}/{{$promotion->qty_total}}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    {{--Expiration Date--}}
+                                    <td class="text-center">
+                                        <div class="widget-content p-0">
+                                            <div class="widget-content-wrapper">
+                                                <div class="widget-content-left flex2">
+{{--                                                    <div--}}
+{{--                                                        class="widget-subheading opacity-10">{{ $ticket->flightSchedule->airportFrom->name . ' > ' . $ticket->flightSchedule->airportTo->name }}</div>--}}
+                                                    <div class="widget-subheading opacity-10">
+                                                        {{ date('H:i d/m/Y', strtotime($promotion->expiration_date))}}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    {{--Action--}}
+                                    <td class="text-center">
+                                        <a href="{{ url()->current() . '/' . $promotion->promotion_id }}"
+                                           class="btn btn-primary btn-hover-shine btn-sm">
+                                            Details
+                                        </a>
+                                        <a href="{{ url()->current() . '/' . $promotion->promotion_id . '/edit'}}"
+                                           data-toggle="tooltip" title="Edit" data-placement="bottom"
+                                           class="btn btn-warning btn-hover-shine btn-sm">
+                                            <span class="btn-icon-wrapper opacity-8">
+                                                <i class="fa fa-edit fa-w-20"></i>
+                                            </span>
+                                        </a>
+                                        <form class="d-inline"
+                                              action="{{ url()->current() . '/' . $promotion->promotion_id }}"
+                                              method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-hover-shine btn-danger btn-sm" type="submit"
+                                                    data-toggle="tooltip" title="Delete" data-placement="bottom"
+                                                    onclick="return confirm('Do you really want to delete this item?')">
+                                                    <span class="btn-icon-wrapper opacity-8">
+                                                        <i class="fa fa-trash fa-w-20"></i>
+                                                    </span>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="d-block card-footer">
+                        {{ $promotions->links() }}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
 @endsection
