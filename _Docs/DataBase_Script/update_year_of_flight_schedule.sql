4cb664060014933145f48dab163620758bbf86b7
Hiếu iceTea - HiếuND <DinhHieu8896@gmail.com>
2023-07-09 02:57:54 +0700

    script update_year_of_flight_schedule.sql

---
+++ _Docs/DataBase_Script/update_year_of_flight_schedule.sql
@@ -0,0 +1,13 @@
+
+# Set the value of the year to change:
+SET @year = '2022';
+
+# execute
+UPDATE flight_schedule
+SET departure_at = DATE_FORMAT(departure_at, CONCAT(@year, '-%m-%d %T')),
+    arrival_at   = DATE_FORMAT(arrival_at, CONCAT(@year, '-%m-%d %T')),
+    updated_at   = CURRENT_TIMESTAMP,
+    version      = version + 1;
+
+# Check database changes:
+SELECT * FROM flight_schedule;
